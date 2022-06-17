785f388c3b0d0d6b86b73746a1e6b00615508da1
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2022-06-17 16:56:59 +0800

    feat: implement style transfer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import StyleTransfer from './style_transfer/Model';
+import {browser} from '@tensorflow/tfjs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [model, setModel] = useState(null);
+    
+    useEffect(() => {
+      if (model == null) {
+        
+        //load tensorflow model using tfjs
+        let load_model = async () => {
+          let style_model = new StyleTransfer();
+          await style_model.load_model()
+          setModel(style_model);
+        }
+        
+        load_model();
+      }
+    }, [model]);
+
+    // perform style transfer
+    const transfer = () => {  
+      let execute = async () => {
+        let result = await model.execute(document.getElementById('styleImage'), document.getElementById('targetImage'));
+        await browser.toPixels(result, document.getElementById('outputImage'));
+
+      }
+
+      execute();
+    }
+
+    return ( 
+      <div>
+        <img id='styleImage' src='./Tsunami_by_hokusai_19th_century.jpg' width="500px" height="500px" alt="Style"/>
+        <img id='targetImage' src='./dlsu_image.jpg' width="500px" height="500px" alt="Target"/>
+        <button onClick={() => transfer()} disabled={!model}>Transfer</button>
+        <canvas id='outputImage'  width="500px" height="500px"/>
+      </div>
+    );
 }
 
 export default App;
